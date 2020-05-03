23706be12f08e95c005faec6772393e2f7241587
Ronaldo <rfeitosa@ciandt.com>
2020-05-03 18:35:19 -0300

    Token JWT

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 import User from '../models/User';
 
@@ -10,6 +11,7 @@ interface Request {
 
 interface Response {
   user: User;
+  token: string;
 }
 
 class AuthenticateUserService {
@@ -29,7 +31,12 @@ class AuthenticateUserService {
       throw new Error(ERROR_MSG);
     }
 
-    return { user };
+    const token = sign({}, '11f0e88ec7d075789084e09363e6b2e3', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    return { user, token };
   }
 }
 
