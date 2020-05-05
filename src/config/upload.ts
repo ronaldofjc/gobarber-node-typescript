024801c3111b55cfe3415026c3bfb6cb9ecf5c9a
Ronaldo <rfeitosa@ciandt.com>
2020-05-05 20:25:42 -0300

    Serving static files

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -6,7 +6,6 @@ const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
 
 export default {
   directory: tmpFolder,
-
   storage: multer.diskStorage({
     destination: tmpFolder,
     filename(request, file, callback) {
